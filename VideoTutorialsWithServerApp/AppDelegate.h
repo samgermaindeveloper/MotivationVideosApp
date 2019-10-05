f2a85339b7b910a25b8a6414e2fefaf1a5d1710a
Sam <sam.germain@usask.ca>
2019-10-05 17:31:04 -0600

    Initial Commit

---
+++ VideoTutorialsWithServerApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoTutorialsWithServerApp
+//
+//  Created by Samuel Germain on 2019-10-05.
+//  Copyright © 2019 Sam G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoTutorialsWithServerApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoTutorialsWithServerApp
+//
+//  Created by Samuel Germain on 2019-10-05.
+//  Copyright © 2019 Sam G. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
